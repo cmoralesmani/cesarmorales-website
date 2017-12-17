e8653b6c33a7e940268dab2015629489743e9d12
Cesar Morales <cmorales@MacBook-Pro-de-Cesar.local>
2017-12-16 22:02:13 -0600

    Movimiento de seccion en la pagina La pagina contara con varias secciones, pero para no utilizar Id en las secciones para ser utilizado con ese fin, entonces se realizo una funcion de javascript que se traslada al lugar indicado referenciadose con el nombre de la clase la cual debe contener la etiqueta <a> en el atributo data-ref.

--- js/website.js
+++ js/website.js
@@ -1,3 +1,15 @@
 $(document).ready(function(e){
   $('#navigation').scrollToFixed();
 });
+
+// Scroll hacia la seccion a la que este indicada en data-ref de la etiqueta a
+$('a').click(function(e){
+  var section = $(this).attr('data-ref');
+  section = '.' + section;
+  if(section){
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $(section).offset().top
+    });
+  }
+});
